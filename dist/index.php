5b4f48dcbcbb377a5614ee96bc65f6b3fe4a3645
Sean <dr.creator@gmail.com>
2023-08-07 22:23:51 +0500

    latest code

--- dist/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include __DIR__.'/../../../../core/html/dist/init.php';
-
-Theme::init('bold');
-Theme::setViewMode('dev');
-Theme::run();
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include __DIR__.'/../../../core/html/init.php';
+
+Theme::init('bold');
+Theme::setViewMode('dev');
+Theme::run();
